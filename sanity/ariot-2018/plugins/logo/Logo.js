c8ae298df14b3e9d0320f46a7382cbb7fb5bfea6
Joakim Myrvoll Johansen <joakimmyrvoll@gmail.com>
2018-03-16 00:07:45 +0100

    feat: add logo

---
+++ sanity/ariot-2018/plugins/logo/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const logoStyle = {
+  "text-align": "center",
+  "font-size": "1.5em",
+};
+
+const Logo = () => <div style={logoStyle}>Itera</div>;
+
+module.exports = Logo;
