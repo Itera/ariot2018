8ab437c4508c2d3f0b70d07386dc296eded372fd
Øyvind Robertsen <oyvind.robertsen@itera.no>
2018-03-16 16:19:30 +0100

    feat: time tracking

--- raspi/coordinator.py
+++ raspi/coordinator.py
@@ -2,12 +2,35 @@
 
 from threading import Thread, Event
 from serial import Serial
+from Queue import Queue
 import time
 import sys
 import requests
+import datetime
+import json
 
 SANITY_API_BASE = "https://k06fkcmv.api.sanity.io/v1/data/"
 SANITY_API_KEY_FILE = 'sanity'
+
+card_event_queue = Queue(maxsize=0)
+
+class DeskState(object):
+    logged_in = False
+    logged_in_timestamp = None
+    sanity_data = {}
+
+    def log_in(self, sanity_data):
+        self.sanity_data = sanity_data
+        print(self.sanity_data)
+        self.logged_in = True
+        self.logged_in_timestamp = datetime.datetime.now()
+        print(sanity_data.get('id') + ' logged in')
+        
+    def log_out(self):
+        self.logged_in = False
+        self.logged_in_timestamp = None 
+        print(self.sanity_data.get('id') + ' logged out')
+        self.sanity_data = None
 
 def read_api_key():
     try:
@@ -23,32 +46,87 @@ def make_auth_header():
     return {'Authorization': 'Bearer ' + SANITY_API_KEY}
 
 def make_get(query):
-    return requests.get(SANITY_API_BASE + "query/production/?query=" + query, headers=make_auth_header())
+    return requests.get(
+        SANITY_API_BASE + "query/production/",
+        params={'query': query},
+        headers=make_auth_header()
+    ).json().get('result', None)
 
 def make_post(body):
-    return requests.post(SANITY_API_BASE + "mutate/production/?returnIds=false&returnDocuments=true&visibility=sync", data=body)
+    return requests.post(
+        SANITY_API_BASE + "mutate/production/?returnIds=false&returnDocuments=true&visibility=sync",
+        json=body,
+        headers=make_auth_header()
+    ).json()
 
-def event_received(event_data):
-    print(make_get('production', "*[_type == 'person']").json())
+def event_received(card_id, state):
+    if not state.logged_in:
+        query = "*[_type == 'person' && id == '{}']".format(card_id)
+        query_result = make_get(query)[0]
+        if query_result is not None:
+            state.log_in(query_result)
+    else:
+        from_timestamp = state.logged_in_timestamp
+        to_timestamp =  datetime.datetime.now()
+        try:
+            state.sanity_data.get('hours')
+            body = {
+                "mutations": [
+                    {
+                        "patch": {
+                            "query": "*[_type == 'person' && id == '{}']".format(card_id),
+                            "insert" : {
+                                "after": "hours[-1]",
+                                "items": [{
+                                    "from": from_timestamp.isoformat(),
+                                    "to": to_timestamp.isoformat()
+                                }]
+                            }
+                        }
+                    }
+                ]
+            }
+            print(make_post(body))
+        except KeyError:
+            body = {
+                "mutations": [
+                    {
+                        "patch": {
+                            "query": "*[_type == 'person' && id == '{}']".format(card_id),
+                            "setIfMissing": {
+                                "hours": [{
+                                    "from": from_timestamp.isoformat(),
+                                    "to": to_timestamp.isoformat()
+                                }]
+                            }
+                        }
+                    }
+                ]
+            }
+            print(make_post(body))
+        state.log_out()
 
 class RFIDReader(Thread):
-    def __init__(self):
+    def __init__(self, queue):
         super(RFIDReader, self).__init__()
+        self.queue = queue
         self.conn = Serial('/dev/ttyUSB0', 9600, 8, 'N', 1, timeout=1)
         self.shutdown_flag = Event()
 
     def run(self):
         while not self.shutdown_flag.is_set():
-            data = self.conn.readline()
+            data = self.conn.readline().strip()
             if data != '':
-                event_received(data)
+                self.queue.put(data)
 
 def main():
+    state = DeskState()
     try:
-        reader = RFIDReader()
+        reader = RFIDReader(card_event_queue)
         reader.start()
         while True:
-            time.sleep(100)
+            if not card_event_queue.empty():
+                event_received(card_event_queue.get(), state)
     except (KeyboardInterrupt, SystemExit):
         reader.shutdown_flag.set()
         reader.join()
