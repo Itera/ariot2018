5e6c23e78ad230eb80ac47e85621a8609b502f85
Thor Martin Abrahamsen <thorma91@gmail.com>
2018-03-16 14:27:38 +0100

    add RFID-reader

---
+++ rfid/readRFID.ino
@@ -0,0 +1,32 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN   D3
+#define SS_PIN    D4
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  mfrc522.PCD_Init();
+}
+
+
+void loop() {
+  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
+    printIdmfrc522.uid.uidByte);
+    delay(1000);
+  }
+}
+
+
+void printId(byte uid[10]) {
+  for(int i = 0; i < sizeof(uid); i++)
+  {
+    Serial.print(uid[i]);
+  }
+  Serial.println();
+}
+
