6c1a7de03a0f825135f3155cd3f4d1bd8291739a
Joakim Myrvoll Johansen <joakimmyrvoll@gmail.com>
2018-03-16 16:23:49 +0100

    feat: listen for new position on serial

--- tableController/README.md
+++ tableController/README.md
@@ -1,3 +1,5 @@
 # Table controller
 
 Used to move the table up and down. This is easily done with two relays that replace the old manual controller.
+
+The arduino receives commands via the Serial connection.
--- tableController/tableController.ino
+++ tableController/tableController.ino
@@ -1,32 +1,77 @@
-int UP = 4;   // D2 (NodeMCU)
-int DOWN = 5; // D1 (NodeMCU)
+// PINS
+int UP = 2;
+int DOWN = 3;
+
+// TABLE CONSTS
+const int lowest = 77;
+const int highest = 123;
+const float cmPerSecond = 4.9;
+const int timeFullMovement = 9500;
+
+// SERIAL
+String inputString = "";
+boolean positionReceived = false;
 
 void setup()
 {
+  Serial.begin(9600);
+  inputString.reserve(3);
   pinMode(UP, OUTPUT);
   pinMode(DOWN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
+  if (positionReceived) {
+    int position = inputString.toInt();
+
+    if(position >= lowest && position <= highest){
+      moveTableTo(position);
+    }
+
+    // clear the string:
+    inputString = "";
+    positionReceived = false;
+  }
+}
+
+void serialEvent() {
+  while(Serial.available()) 
+   {
+      char ch = Serial.read();
+      
+      if(isDigit(ch)) {
+        inputString += ch;
+      }
+      
+      if(ch == '\n') {
+        positionReceived = true; 
+      }
+   }
+}
+
+void moveTableTo(int height){
+  toBottom();
   moveTable(UP);
-  delay(1000);
-  moveTable(DOWN);
-  delay(1000);
+  delay(((height-lowest)/cmPerSecond)*1000);
   stopMovement();
-  delay(1000);
+}
+
+void toBottom(){
+  moveTable(DOWN);
+  delay(timeFullMovement);  
+  stopMovement();
 }
 
 void moveTable(int dir){
-  int oppositeDir = dir == UP ? DOWN : UP;
-  
-  digitalWrite(oppositeDir, LOW);
-  delay(500);
+  stopMovement();
   digitalWrite(dir, HIGH);
 }
 
 void stopMovement(){
   digitalWrite(UP, LOW);
   digitalWrite(DOWN, LOW);
+  delay(500);
 }
 
